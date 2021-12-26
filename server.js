2bdb25da697a78c707025e7d39f7be449adade1f
Yash Garg <yash.yg.garg111@gmail.com>
2021-12-26 22:20:23 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+
+const port = process.env.PORT || 3030
+
+//uuid: creates uinque URL/ unique id for each room
+const { v4: uuidv4 } = require("uuid");
+
+//EJS: embedded js | templating lang.
+app.set("view engine", "ejs");  //setting up template engine
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+//PeerJS allow us to implement WebRTC.
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.use("/peerjs", peerServer);
+
+app.use(express.static("public"));  //to access css
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`); //redirecting to new URL
+});
+
+//redering room.ejs
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });  //passing the unique room(url)
+});
+
+io.on("connection", (socket) => {
+  console.log('new Websocket Connection');
+
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+  });
+});
+
+server.listen(port, () => {
+  console.log('server is up on port', port);
+});
